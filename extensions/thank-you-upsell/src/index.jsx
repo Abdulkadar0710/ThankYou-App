f062c8cdc1809a4813406ecff128a53919a260ca
Abdul Kadar Khan <abdul.khan@galaxyweblinks.com>
2026-09-17 19:16:12 +0530

    Add read_order_edits and write_order_edits scopes to shopify.app.toml

--- extensions/thank-you-upsell/src/index.jsx
+++ extensions/thank-you-upsell/src/index.jsx
@@ -232,6 +232,8 @@ function UpsellProductCard({product, timerExpired, orderConfirmation}) {
         quantity: 1,
       });
 
+      console.log("response: ", response);
+
       if (response?.success) {
         setAdded(true);
         setStatusTone('success');
